304d71e1b3ce393d61f06603bb338da5f636121b
Daniel-Fonseca-da-Silva <daniel117fonseca@gmail.com>
2025-05-21 12:54:11 +0100

    fix: Added module of traffic and env configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TrafficModule } from './modules/traffic/traffic.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TrafficModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
